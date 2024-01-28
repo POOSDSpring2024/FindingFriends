cfd2fd7b66b792b22a54b9fd5ea6fb45e19b7081
Rickory2021 <rickylego15@gmail.com>
2024-01-27 21:32:40 -0500

    Edited Edit and Naming convention

--- LAMPAPI/Edit.php
+++ LAMPAPI/Edit.php
@@ -8,12 +8,16 @@
     $lastName = $inData["lastName"];
     $phone = $inData["phone"];
     $email = $inData["email"];
-    $userID = $inData["userID"];
+    //$userID = $inData["userID"];
+    //Since we can't know which Contact to change, we need to send the Contact ID, not the User ID
+    //For example, what would happen if first,last,phone, and email are changed, then we wouldn't know which contact to change
+    $id = $inData["id"];
 
     $conn = new mysqli("localhost", "MasterUser", "COP4331TwoFour", "COP4331");
     if ($conn->connect_error) {
         returnWithError($conn->connect_error);
     } else {
+        // The SQL below is searching for the Contact's ID, not the UserID; hence why we changed $userID to $id
         $stmt = $conn->prepare("SELECT * FROM Contacts WHERE ID = ?");
         $stmt->bind_param("i", $inData['id']);
         $stmt->execute();
@@ -24,8 +28,8 @@
         if (!$row) {
             returnWithError("Record not found");
         } else {
-            $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=?, UserID=? WHERE ID=?");
-            $stmt->bind_param("sssssi", $firstName, $lastName, $phone, $email, $userID, $inData['id']);
+            $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=?, /*UserID=?*/ WHERE ID=?");
+            $stmt->bind_param(/*"sssssi"*/"ssssi", $firstName, $lastName, $phone, $email, /*$userID,*/ $inData['id']);
             $stmt->execute();
 
             $stmt->close();
